456ee965a95e454e7219aae99283c3d98ded1693
Karlygash Yakiyayeva <53092602+karlakz@users.noreply.github.com>
2022-07-27 20:47:50 +0600

    [ref]: Clean garbage

--- sortArrObjProperties.js
+++ sortArrObjProperties.js
@@ -1,5 +1,3 @@
-// To sort an array objects based on its properties in the ascending order, we need to use sort() method. Keep in mind, that sort() method mutates the original array, hence we can make the shallow copy of the original array firts and make changes to it.
-
 const dogs = [
   { weight: 22, curFood: 250, owners: ["Karla", "Max"] },
   { weight: 8, curFood: 200, owners: ["Anelya"] },
@@ -8,7 +6,3 @@ const dogs = [
 ];
 
 console.log(dogs.slice().sort((a, b) => a.curFood - b.curFood));
-
-// Reference:
-// 1. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-// 2. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
