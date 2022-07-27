d3369e585669aa16d3b66afcf6539bad2e59fa9a
Karlygash Yakiyayeva <53092602+karlakz@users.noreply.github.com>
2022-07-27 20:59:57 +0600

    [ref]: Clean garbage

--- loopOverArrObjects.js
+++ loopOverArrObjects.js
@@ -1,4 +1,3 @@
-// How to loop over the array objects, make calculations and add it as a new property to the object array/ For example:
 const dogs = [
   { weight: 22, curFood: 250, owners: ["Karla", "Max"] },
   { weight: 8, curFood: 200, owners: ["Anelya"] },
@@ -13,6 +12,3 @@ const recommendedFoodPortion = function (dogs) {
 };
 recommendedFoodPortion(dogs);
 console.log(dogs);
-
-// References:
-// 1. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
